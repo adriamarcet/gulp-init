a06585c3448bc852ef9122c5024185e312906ab3
Adrià Marcet <amarcet@runroom.com>
2019-04-21 14:49:24 +0200

    Add autoprefixer to styles task

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -2,14 +2,10 @@
 // const gulp = require('gulp');
 //Con babel:
 import gulp from 'gulp';
-import {sass} from './tasks/styles.base.js';
+import { styles } from './tasks/styles.base.js';
 import lintCss from './tasks/styles.lint.js';
 import minifyCss from './tasks/styles.minify.js';
 
-const buildStyles = gulp.series(lintCss, sass, minifyCss);
+const buildStyles = gulp.series(lintCss, styles, minifyCss);
 
 gulp.task('default', buildStyles);
-
-// To do
-// css-nano configurar sin z-index
-// autoprefixer
--- gulpfile.babel.js/tasks/styles.base.js
+++ gulpfile.babel.js/tasks/styles.base.js
@@ -2,13 +2,20 @@ import gulp from 'gulp';
 import gulpsass from 'gulp-sass';
 import routes from '../routes.js';
 
+import postcss from 'gulp-postcss';
+import sourcemaps from 'gulp-sourcemaps';
+import autoprefixer from 'autoprefixer';
+
 const STYLE_FILES = `${routes.scss}**/*.scss`;
 const STYLE_FILES_COMPILED = `${routes.dest}assets/css`;
 
-const sass = function () {
+const styles = function () {
     return gulp.src(STYLE_FILES)
       .pipe(gulpsass().on('error', gulpsass.logError))
+      .pipe(sourcemaps.init())
+      .pipe(postcss([autoprefixer()]))
+      .pipe(sourcemaps.write('.'))
       .pipe(gulp.dest(STYLE_FILES_COMPILED));
 }
 
-export { STYLE_FILES, STYLE_FILES_COMPILED, sass };
+export { STYLE_FILES, STYLE_FILES_COMPILED, styles };

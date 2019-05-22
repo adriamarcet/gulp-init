b78895bd00c66a5692a5e4da6529b6c55222e6d3
Adria <adriamarcetrovira@gmail.com>
2019-05-23 00:53:03 +0200

    Add watcher to styles task

--- gulpfile.babel.js/index.js
+++ gulpfile.babel.js/index.js
@@ -2,12 +2,12 @@
 // const gulp = require('gulp');
 //Con babel:
 import gulp from 'gulp';
-import { styles } from './tasks/styles.base.js';
+import { watchStyles } from './tasks/styles.base.js';
 import minifyCss from './tasks/styles.minify.js';
 import fonts from './tasks/fonts.js';
 import scripts from './tasks/scripts.js';
 
-const buildStyles = gulp.series(styles, minifyCss);
+const buildStyles = gulp.series(watchStyles, minifyCss);
 const buildScripts = gulp.series(scripts);
 
 gulp.task('default', gulp.parallel(fonts, buildStyles, buildScripts));
--- gulpfile.babel.js/tasks/styles.base.js
+++ gulpfile.babel.js/tasks/styles.base.js
@@ -25,4 +25,10 @@ const styles = function () {
       .pipe(gulp.dest(STYLE_FILES_COMPILED));
 }
 
-export { STYLE_FILES, STYLE_FILES_COMPILED, styles };
+const watchStyles = function() {
+  styles();
+
+  return gulp.watch(STYLE_FILES, styles);
+}
+
+export { STYLE_FILES, STYLE_FILES_COMPILED, watchStyles };

69610347454d7d075f4df4c2659a2f290b9f7ac4
Adrià Marcet <amarcet@runroom.com>
2019-06-12 16:12:09 +0200

    Add language logic

--- gulpfile.babel.js/tasks/markup.js
+++ gulpfile.babel.js/tasks/markup.js
@@ -1,15 +1,38 @@
 import gulp from 'gulp';
 import twig from 'gulp-twig';
+
 import { MARKUP_SRC, MARKUP_DEST } from '../routes.js';
 
-const markup = function () {
+import DATA_EN from '../../data/en/translations.json';
+import DATA_ES from '../../data/es/translations.json';
+
+const languages = {
+    es: DATA_ES,
+    en: DATA_EN
+};
+
+const defaultLanguage = 'en';
+
+const generateMarkup = (locale, t) => {
+    const route = locale === defaultLanguage ? '' : `/${locale}`;
     return gulp
         .src(`${MARKUP_SRC}`)
         .pipe(twig({
             cache: false,
-            base: `app/resources`,
+            base: 'app/resources',
+            data: {
+                languages: languages,
+                t
+            }
         }))
-        .pipe(gulp.dest(`${MARKUP_DEST}`));
+        .pipe(gulp.dest(`${MARKUP_DEST}${route}/`));
+}
+
+const markup = function () {
+    Object.keys(languages).map(actual => {
+        console.log(actual);
+        return generateMarkup(actual, languages[actual]);
+    });
 }
 
 const MARKUP_FILES = MARKUP_SRC;
